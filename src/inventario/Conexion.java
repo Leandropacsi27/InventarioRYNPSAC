04dbee061b1f3c7361659540de3c4fd036fd8d7c
Leandro <Leandro@DESKTOP-5KGI4TO>
2023-07-21 13:20:09 -0500

    Primer commit

---
+++ src/inventario/Conexion.java
@@ -0,0 +1,20 @@
+package inventario;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+
+public class Conexion {
+    Connection cn;
+    
+    public Connection conectar() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            cn=DriverManager.getConnection("jdbc:mysql://localhost:3306/base_datos","root","");
+            JOptionPane.showMessageDialog(null, "La conexion a la base de datos fue exitosa");
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "La conexion a la base de datos no fue exitosa"+ e.toString());
+        }
+        return cn;
+    }
+}
