d9d818d2e32e95321fd5a9c6bd92b1916729901c
Leandro <Leandro@DESKTOP-5KGI4TO>
2023-07-29 17:31:26 -0500

    Limpiando inventario, dejando una sola clase como ejecutable, Clase Main como ejecutable-.

--- src/inventario/Main.java
+++ src/inventario/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Ingresar form = new Ingresar();
         form.setVisible(true);
+        form.setLocationRelativeTo(null);
     }
 }
 /*hola mundo*/
